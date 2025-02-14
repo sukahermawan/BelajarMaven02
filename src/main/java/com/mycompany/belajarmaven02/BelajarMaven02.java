9bc87d75e720e84495a03874567663a2b42497a5
Suka Hermawan net <sukahermawan877@gmail.com>
2025-02-14 14:28:32 +0000

    add new file

---
+++ src/main/java/com/mycompany/belajarmaven02/BelajarMaven02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.belajarmaven02;
+
+/**
+ *
+ * @author sukah
+ */
+public class BelajarMaven02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
